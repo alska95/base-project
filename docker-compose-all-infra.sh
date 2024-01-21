b7d5cec1cfe1bc6d6cb97075cf110d72fd12d2b5
alska95 <rhsalska55@naver.com>
2024-01-21 16:49:08 +0900

    [CloudGateway] 경로명 정리

--- docker-compose-all-infra.sh
+++ docker-compose-all-infra.sh
@@ -6,11 +6,11 @@ COMPOSE_INFRA_FILE=docker-compose-all-infra.yml
 if [ "$1" = "start" ]; then
     # Docker Compose로 서비스 시작
     echo "Starting services..."
-    docker-compose -f COMPOSE_INFRA_FILE up -d
+    docker-compose -f $COMPOSE_INFRA_FILE up -d
 elif [ "$1" = "stop" ]; then
     # Docker Compose로 서비스 중지
     echo "Stopping services..."
-    docker-compose -f COMPOSE_INFRA_FILE down
+    docker-compose -f $COMPOSE_INFRA_FILE down
 else
     echo "Unknown command '$1'. Use 'start' or 'stop'."
 fi

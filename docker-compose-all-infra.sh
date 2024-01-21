c90250527f9735765f98e5e09492029286e2c785
alska95 <rhsalska55@naver.com>
2024-01-21 16:01:53 +0900

    [Docker] docker-compose로 인프라 컨테이너 한번에 실행, 로컬에선 localhost ip사용하도록 변경

---
+++ docker-compose-all-infra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Docker Compose 파일의 경로
+COMPOSE_INFRA_FILE=docker-compose-all-infra.yml
+
+if [ "$1" = "start" ]; then
+    # Docker Compose로 서비스 시작
+    echo "Starting services..."
+    docker-compose -f COMPOSE_INFRA_FILE up -d
+elif [ "$1" = "stop" ]; then
+    # Docker Compose로 서비스 중지
+    echo "Stopping services..."
+    docker-compose -f COMPOSE_INFRA_FILE down
+else
+    echo "Unknown command '$1'. Use 'start' or 'stop'."
+fi
